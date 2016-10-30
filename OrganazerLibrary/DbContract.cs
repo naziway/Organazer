f15e0fbf64b7a0f35e9350412c3b1a1d93bb0d15
nazar.kravchuk <nazar.kravchuk@terrasoft.net.ua>
2016-10-30 21:44:30 +0200

    Interface for db added.

--- OrganazerLibrary/DbContract.cs
+++ OrganazerLibrary/DbContract.cs
@@ -1,7 +1,32 @@
-﻿namespace OrganazerLibrary
+﻿using System.Collections.Generic;
+
+namespace OrganazerLibrary
 {
-    public class DbContract
+    public class DbContract:IContract
     {
-         
+        public List<Taska> GetAllTaskas()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public List<Taska> GetTaskasForDay(DayOfWeek dayOfWeek)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public List<Taska> GetTaskasForSubject(int idSubject)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public List<Subject> GetAllSubjects()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public List<Subject> GetScheduleInDay()
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
---
+++ OrganazerLibrary/IContract.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace OrganazerLibrary
+{
+    public interface IContract
+    {
+        List<Taska> GetAllTaskas();
+        List<Taska> GetTaskasForDay(DayOfWeek dayOfWeek);
+        List<Taska> GetTaskasForSubject(int idSubject);
+        List<Subject> GetAllSubjects();
+        List<Subject> GetScheduleInDay();
+    }
+}
